ccabb70e3d097d8128557a083d826d78f7883c12
Ogbotic <innosonnice@gmail.com>
2022-10-17 12:35:52 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program
+ * Return: 0
+ */
+
+int main(void)
+{
+	char lu;
+
+	for (lu = '0'; lu <= '9'; lu++)
+	{
+		putchar(lu);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
